0a954d44813b86a6e78f850da9d5d29c55b4ac8e
dan9112_work_KamAZ <bessonovdanil9112@mail.ru>
2021-04-07 14:44:59 +0300

    simple service

--- p0921_service_simple/src/main/java/ru/example/androidlessons1/MainActivity.java
+++ p0921_service_simple/src/main/java/ru/example/androidlessons1/MainActivity.java
@@ -1,14 +1,24 @@
 package ru.example.androidlessons1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+    }
+
+    public void onClickStart(View view) {
+        startService(new Intent(this, MyService.class));
+    }
+
+    public void onClickStop(View view) {
+        stopService(new Intent(this, MyService.class));
     }
 }
