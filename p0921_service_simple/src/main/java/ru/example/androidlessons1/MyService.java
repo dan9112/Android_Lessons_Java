3637a971d4447166f1651aea883e958905116c9b
dan9112_work_KamAZ <bessonovdanil9112@mail.ru>
2021-04-07 14:44:59 +0300

    simple service

---
+++ p0921_service_simple/src/main/java/ru/example/androidlessons1/MyService.java
@@ -0,0 +1,37 @@
+package ru.example.androidlessons1;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyService extends Service {
+
+    private static final String LOG_TAG = "myLogs";
+
+    public void onCreate() {
+        super.onCreate();
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG_TAG, "onStartCommand");
+        someTask();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    public IBinder onBind(Intent intent) {
+        Log.d(LOG_TAG, "onBind");
+        return null;
+    }
+
+    private void someTask() {
+        Toast.makeText(this, "Wow!", Toast.LENGTH_SHORT).show();
+    }
+}
