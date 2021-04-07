aa2c4273741f97d10b108f24d3c5f9563f3a4dcc
dan9112_work_KamAZ <bessonovdanil9112@mail.ru>
2021-04-07 15:56:18 +0300

    simple service 2.0
    Added code with its own example of a service task, documentation.
    Renamed application.

--- p0921_service_simple/src/main/java/ru/example/androidlessons1/MyService.java
+++ p0921_service_simple/src/main/java/ru/example/androidlessons1/MyService.java
@@ -7,45 +7,74 @@ import android.util.Log;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Простой пример сервиса
+ */
 public class MyService extends Service {
 
+    /**
+     * Тег логов сервиса
+     */
     private static final String LOG_TAG = "myLogs";
 
+    /**
+     * Флаг остановки потока задачи сервиса
+     */
+    private static boolean stop = false;
+
+    @Override
     public void onCreate() {
         super.onCreate();
         Log.d(LOG_TAG, "onCreate");
     }
 
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(LOG_TAG, "onStartCommand");
         someTask();
         return super.onStartCommand(intent, flags, startId);
     }
 
+    @Override
     public void onDestroy() {
         super.onDestroy();
+        stop = !stop;
         Log.d(LOG_TAG, "onDestroy");
     }
 
+    @Override
     public IBinder onBind(Intent intent) {
         Log.d(LOG_TAG, "onBind");
         return null;
     }
 
+    /**
+     * Функция с кодом нашего сервиса
+     */
     private void someTask() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 1; i <= 5; i++) {
-                    Log.d(LOG_TAG, "i = " + i);
-                    try {
-                        TimeUnit.SECONDS.sleep(1);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        new Thread(() -> {
+            // запуск бесконечного цикла выводов в лог похожих строк с задержкой в 1 секунду
+            int i = 0;
+            while (!stop) {
+                i++;
+                Log.d(LOG_TAG, "Log [" + i + "]");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                stopSelf();
             }
+
+            // Вывод чисел от одного до 5 с задержкой в 1 секунду
+            // for (int i = 1; i <= 5; i++) {
+            //     Log.d(LOG_TAG, "i = " + i);
+            //     try {
+            //         TimeUnit.SECONDS.sleep(1);
+            //     } catch (InterruptedException e) {
+            //         e.printStackTrace();
+            //     }
+            // }
+            // stopSelf();
         }).start();
     }
 }
