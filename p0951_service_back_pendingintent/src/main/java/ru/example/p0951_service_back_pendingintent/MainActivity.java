738e8695c95a0d770acc6dda69a8ea37a719a895
dan9112_work_KamAZ <bessonovdanil9112@mail.ru>
2021-04-09 09:21:53 +0300

    service back pendingintent 1.0
    Added example

---
+++ p0951_service_back_pendingintent/src/main/java/ru/example/p0951_service_back_pendingintent/MainActivity.java
@@ -0,0 +1,164 @@
+package ru.example.p0951_service_back_pendingintent;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Основная активность приложения.
+ * <p>
+ * Запускает сервис {@link MyService}, взаимодействует с ним, отправляя и получая данные
+ * </p>
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Код для типа возвращаемого сообщения "Задача начала работу"
+     */
+    public final static int STATUS_START = 100,
+    /**
+     * Код для типа возвращаемого сообщения "Задача завершила работу"
+     */
+    STATUS_FINISH = 200;
+
+    /**
+     * Ключ для передаваемого параметра "Время задержки"
+     */
+    public final static String PARAM_TIME = "time",
+    /**
+     * Ключ для передаваемого параметра {@link PendingIntent} для предоставления права
+     * взаимодействия с текущей активностью
+     */
+    PARAM_PINTENT = "pendingIntent",
+    /**
+     * Ключ для возвращаемого параметра "Результат"
+     */
+    PARAM_RESULT = "result";
+
+    /**
+     * Тег логов класса
+     */
+    private final static String LOG_TAG = "myLogs";
+
+    /**
+     * Код запроса для Task1
+     */
+    final int TASK1_CODE = 1,
+    /**
+     * Код запроса для Task2
+     */
+    TASK2_CODE = 2,
+    /**
+     * Код запроса для Task3
+     */
+    TASK3_CODE = 3;
+
+    /**
+     * Текстовое окно вывода для Task1
+     */
+    TextView tvTask1,
+    /**
+     * Текстовое окно вывода для Task2
+     */
+    tvTask2,
+    /**
+     * Текстовое окно вывода для Task3
+     */
+    tvTask3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        tvTask1 = findViewById(R.id.tvTask1);
+        tvTask1.setText("Task1");
+
+        tvTask2 = findViewById(R.id.tvTask2);
+        tvTask2.setText("Task2");
+
+        tvTask3 = findViewById(R.id.tvTask3);
+        tvTask3.setText("Task3");
+    }
+
+    /**
+     * Функция обработки нажатия на кнопку запуска.
+     * <p>
+     * Запускает серию вызовов сервиса {@link MyService} с различными входными данными
+     * </p>
+     *
+     * @param view виджет кнопки
+     */
+    public void onClickStart(View view) {
+        PendingIntent pi;
+        Intent intent;
+
+        // Создаём PendingIntent для Task1
+        pi = createPendingResult(TASK1_CODE, new Intent(), 0);
+        // Создаём Intent для вызова сервиса, кладём туда параметр времени и созданный PendingIntent
+        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 7)
+                .putExtra(PARAM_PINTENT, pi);
+        // Запускаем сервис
+        startService(intent);
+
+        // Создаём PendingIntent для Task2
+        pi = createPendingResult(TASK2_CODE, new Intent(), 0);
+        // Создаём Intent для вызова сервиса, кладём туда параметр времени и созданный PendingIntent
+        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 4)
+                .putExtra(PARAM_PINTENT, pi);
+        // Запускаем сервис
+        startService(intent);
+
+        // Создаём PendingIntent для Task3
+        pi = createPendingResult(TASK3_CODE, new Intent(), 0);
+        // Создаём Intent для вызова сервиса, кладём туда параметр времени и созданный PendingIntent
+        intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 6)
+                .putExtra(PARAM_PINTENT, pi);
+        // Запускаем сервис
+        startService(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(LOG_TAG, "requestCode = " + requestCode + ", resultCode = "
+                + resultCode);
+
+        // Ловим сообщения о запуске задач
+        if (resultCode == STATUS_START) {
+            switch (requestCode) {
+                case TASK1_CODE:
+                    tvTask1.setText("Task1 start");
+                    break;
+                case TASK2_CODE:
+                    tvTask2.setText("Task2 start");
+                    break;
+                case TASK3_CODE:
+                    tvTask3.setText("Task3 start");
+                    break;
+            }
+        }
+
+        // Ловим сообщения об окончании задач
+        if (resultCode == STATUS_FINISH) {
+            int result = data.getIntExtra(PARAM_RESULT, 0);
+            switch (requestCode) {
+                case TASK1_CODE:
+                    tvTask1.setText("Task1 finish, result = " + result);
+                    break;
+                case TASK2_CODE:
+                    tvTask2.setText("Task2 finish, result = " + result);
+                    break;
+                case TASK3_CODE:
+                    tvTask3.setText("Task3 finish, result = " + result);
+                    break;
+            }
+        }
+    }
+}
---
+++ p0951_service_back_pendingintent/src/main/java/ru/example/p0951_service_back_pendingintent/MyService.java
@@ -0,0 +1,119 @@
+package ru.example.p0951_service_back_pendingintent;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class MyService extends Service {
+
+    /**
+     * Тег логов класса
+     */
+    private final static String LOG_TAG = "myLogs";
+
+    /**
+     * Экземпляр интерфейса для управления исполнением и контроля потоков
+     */
+    private ExecutorService es;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.d(LOG_TAG, "MyService onCreate");
+        es = Executors.newFixedThreadPool(2);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "MyService onDestroy");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG_TAG, "MyService onStartCommand");
+
+        int time = intent.getIntExtra(MainActivity.PARAM_TIME, 1);
+        PendingIntent pi = intent.getParcelableExtra(MainActivity.PARAM_PINTENT);
+
+        MyRun mr = new MyRun(time, startId, pi);
+        es.execute(mr);
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    /**
+     * Свой класс с реализацией интерфейса
+     */
+    private class MyRun implements Runnable {
+
+        /**
+         * Время задержки
+         */
+        private final int time,
+        /**
+         * Порядковый номер запроса сервиса
+         */
+        startId;
+
+        /**
+         * Объект для связи с активностью, сделавшей запрос
+         */
+        PendingIntent pi;
+
+        /**
+         * Конструктор получения необходимых параметров. Выводит в лог строку с сообщением о
+         * создании объекта
+         *
+         * @param time    время задержки
+         * @param startId порядковый номер запроса сервиса
+         * @param pi      объект для связи с активностью, сделавшей запрос
+         */
+        public MyRun(int time, int startId, PendingIntent pi) {
+            this.time = time;
+            this.startId = startId;
+            this.pi = pi;
+            Log.d(LOG_TAG, "MyRun#" + startId + " create");
+        }
+
+        @Override
+        public void run() {
+            Log.d(LOG_TAG, "MyRun#" + startId + " start, time = " + time);
+            try {
+                // Сообщаем о запуске задачи
+                pi.send(MainActivity.STATUS_START);
+
+                // Начинаем выполнение задачи
+                TimeUnit.SECONDS.sleep(time);
+
+                // Сообщаем об окончании задачи
+                Intent intent = new Intent().putExtra(MainActivity.PARAM_RESULT, time * 100);
+                pi.send(MyService.this, MainActivity.STATUS_FINISH, intent);
+            } catch (InterruptedException | PendingIntent.CanceledException e) {
+                e.printStackTrace();
+            }
+            stop();
+        }
+
+        /**
+         * Функция, информирующая сервис о завершении выполнения команды по её {@link #startId}.
+         * Также выводит информацию о сыбытии в лог
+         */
+        void stop() {
+            Log.d(LOG_TAG, "MyRun#" + startId + " end, stopSelfResult(" + startId + ") = "
+                    + stopSelfResult(startId));
+        }
+    }
+}
