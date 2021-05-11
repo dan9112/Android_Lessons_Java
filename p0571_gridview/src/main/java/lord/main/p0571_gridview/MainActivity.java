2fedb8a0d9183d52ace96869f5ac21da3598c2b3
dan9112_work_KamAZ <bessonovdanil9112@mail.ru>
2021-05-11 10:32:52 +0300

    default
    Заполнение GridView по умолчанию

---
+++ p0571_gridview/src/main/java/lord/main/p0571_gridview/MainActivity.java
@@ -0,0 +1,31 @@
+package lord.main.p0571_gridview;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] data = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+            "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+    GridView gvMain;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        adapter = new ArrayAdapter<>(this, R.layout.item, R.id.tvText, data);
+        gvMain = findViewById(R.id.gvMain);
+        gvMain.setAdapter(adapter);
+        adjustGridView();
+    }
+
+    private void adjustGridView() {
+
+    }
+}
