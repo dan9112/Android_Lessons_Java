39557340c8451ba8d2455d3a3b2351e2b732eaf5
dan9112_work_KamAZ <bessonovdanil9112@mail.ru>
2021-07-12 08:41:41 +0300

    исправление ошибок
    Исправлены ошибки обнуления счётчика, изменён тип данных флага
    остановки/паузы/запуска счётчика с Boolean на UShort, добавлен
    внутренний счётчик кол-ва сделанных логов, изменён тип данных лога со
    String на MutableLiveData<String> и добавление его прослушивателя

--- t2_viewmodel/src/main/java/com/example/t2_viewmodel/BaseFragment.kt
+++ t2_viewmodel/src/main/java/com/example/t2_viewmodel/BaseFragment.kt
@@ -26,27 +26,26 @@ class BaseFragment : Fragment() {
             binding = DataBindingUtil.inflate(inflater, R.layout.fragment_base, container, false)
             viewModel = ViewModelProvider(this@BaseFragment).get(BaseViewModel::class.java)
 
-            binding.logWindow.text = viewModel.log
-
             viewModel.counter.observe(viewLifecycleOwner, { currentNumber ->
                 binding.display.text = String.format("%.2f", currentNumber.toFloat() / 100)
+            })
+
+            viewModel.log.observe(viewLifecycleOwner, { currentLog ->
+                binding.logWindow.text = currentLog
             })
 
             binding.start.setOnClickListener {
                 viewModel.startCount()
             }
             binding.start.setOnLongClickListener {
-                viewModel.started = false
-                viewModel.counter.value = 0
+                viewModel.resetCounter()
                 return@setOnLongClickListener true
             }
             binding.log.setOnClickListener {
-                viewModel.log = binding.display.text.toString() + "\n" + viewModel.log
-                binding.logWindow.text = viewModel.log
+                viewModel.updateLog(binding.display.text.toString())
             }
             binding.log.setOnLongClickListener {
-                viewModel.log = ""
-                binding.logWindow.text = ""
+                viewModel.clearLog()
                 return@setOnLongClickListener true
             }
         }
--- t2_viewmodel/src/main/java/com/example/t2_viewmodel/BaseViewModel.kt
+++ t2_viewmodel/src/main/java/com/example/t2_viewmodel/BaseViewModel.kt
@@ -1,33 +1,76 @@
 package com.example.t2_viewmodel
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class BaseViewModel : ViewModel() {
 
-    var counter = MutableLiveData<Int>()
+    /** Счётчик (локальная переменная класса) */
+    private val _counter = MutableLiveData<Int>()
 
-    var started = false
+    /** Счётчик (публичная переменная ReadOnly) */
+    val counter: LiveData<Int>
+        get() = _counter
 
-    var log = ""
+    /** Счётчик отмеченных в логе строк */
+    private var logCounter = 1
+
+    /**
+     * Флаг секундомера.
+     *
+     * Коды: 0 - пауза, 1 - работа, 2 - остановка
+     */
+    private var started: UShort = 2u
+
+    /** Непосредственно секундомер */
+    private lateinit var job: Job
+
+    /** Лог (локальная переменная класса) */
+    private val _log = MutableLiveData<String>()
+
+    /** Лог (публичная переменная ReadOnly) */
+    val log: LiveData<String>
+        get() = _log
 
     init {
-        counter.value = 0
+        _counter.value = 0
+        _log.value = ""
         Log.i("BaseViewModel", "BaseViewModel created!")
     }
 
+    /** Функция счёта секундомера */
     fun startCount() {
-        started = !started
-        GlobalScope.launch(Dispatchers.Main) {
-            while (started) {
-                delay(10)
-                counter.value = counter.value!! + 1
-            }
+        when (started) {
+            0u.toUShort(), 2u.toUShort() -> started = 1u
+            1u.toUShort() -> started = 0u
         }
+
+        job = GlobalScope.launch(Dispatchers.Main) {
+            while (started == 1u.toUShort()) {
+                delay(10)
+                _counter.value = counter.value!! + 1
+            }
+            if (started == 2u.toUShort()) _counter.value = 0
+        }
+    }
+
+    fun clearLog() {
+        _log.value = ""
+        logCounter = 1
+    }
+
+    fun resetCounter() {
+        started = 2u
+        if (job.isCompleted) {
+            _counter.value = 0
+        }
+    }
+
+    fun updateLog(currentLink: String) {
+        _log.value = log.value + "\n$logCounter: $currentLink"
+        logCounter++
     }
 }
